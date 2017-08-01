488eba87b763cc0492a3136cb95bb93f1dfcdbda
siddhant3030 <ssiddhant3030@gmail.com>
2017-08-01 17:20:43 +0530

    first commit

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @product = products(:one)
+    @update = {
+      title: 'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url: 'Lorem.jpg',
+      price: 300
+    }
+  end
+
+  test "should get index" do
+    get products_url
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
+
+  test "should get new" do
+    get new_product_url
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      post :create, product: @update
+    end
+
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  test "should show product" do
+    get product_url(@product)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_product_url(@product)
+    assert_response :success
+  end
+
+  test "should update product" do
+  patch :update, id: @product, product: @update
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+
+    assert_redirected_to products_url
+  end
+end
