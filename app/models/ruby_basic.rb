d4b7eb488e27df7f16234082834fd1fc3e8f03d6
Siddhant Singh <zuppler@Siddhants-MacBook-Pro.local>
2022-01-29 22:53:16 +0530

    ruby basic

---
+++ app/models/ruby_basic.rb
@@ -0,0 +1,22 @@
+# puts "Enter a number: "
+# num1 = gets.chomp()
+# puts "Enter another number"
+# num2 = gets.chomp()
+
+# puts(num1.to_i + num2.to_i)
+
+# Hashes 
+# states = {
+#     "keu" => "value", 
+#     :key2 => "value",
+#     "key3" => "value"
+# }
+
+# puts states["keu"]
+# puts states[:key2]
+
+
+# a = {
+#     "content" => [{"a" => 1, "b" => 2}]
+# }
+
